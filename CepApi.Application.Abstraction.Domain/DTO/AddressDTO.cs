501e7b6db0f38e822f514b557ca05db309875656
Kostylll <pedromontevanderlei@gmail.com>
2025-08-25 13:01:16 -0300

    NuGet Packages and DTO/Entity Added

---
+++ CepApi.Application.Abstraction.Domain/DTO/AddressDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViaCep.Application.Abstraction.Domain.DTO
+{
+    public class AddressDTO
+    {
+        public string? Cep {  get; set; }
+        public string? Logradouro { get; set; }
+        public string? Bairro { get; set; }
+        public string? Localidade { get; set; }
+        public string? Uf { get; set; }
+
+
+
+    }
+}
