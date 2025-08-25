02c0d7c67f5d0e6a90397c4a278987f2508dca3b
Kostylll <pedromontevanderlei@gmail.com>
2025-08-25 14:32:22 -0300

    Simple Unit Test Added

---
+++ CepApi.Application.UnitTests/CepUnitTest.cs
@@ -0,0 +1,34 @@
+using Newtonsoft.Json;
+using CepApi.Application.Abstraction.Domain.Models;
+
+namespace CepApi.Application.UnitTests
+{
+    public class CepUnitTest
+    {
+        [Fact]
+        public void DeveDesserializarJsonDeCepCorretamente()
+        {
+            var json = @"{
+                ""cep"": ""01001-000"",
+                ""logradouro"": ""Praça da Sé"",
+                ""complemento"": ""lado ímpar"",
+                ""bairro"": ""Sé"",
+                ""localidade"": ""São Paulo"",
+                ""uf"": ""SP"",
+                ""ibge"": ""3550308"",
+                ""gia"": ""1004"",
+                ""ddd"": ""11"",
+                ""siafi"": ""7107""
+            }";
+
+            var address = JsonConvert.DeserializeObject<Address>(json);
+
+            Assert.NotNull(address);
+            Assert.Equal("01001-000", address.Cep);
+            Assert.Equal("Praça da Sé", address.Logradouro);
+            Assert.Equal("São Paulo", address.Localidade);
+            Assert.Equal("SP", address.Uf);
+        }
+    }
+
+}
